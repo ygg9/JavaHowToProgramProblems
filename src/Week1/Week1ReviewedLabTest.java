0c2b921eb4aca51adb207d99c19c83a6351966cd
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-21 14:26:03 -0500

    moved test to same package as lab

--- src/Week1/Week1ReviewedLabTest.java
+++ src/Week1/Week1ReviewedLabTest.java
@@ -1,10 +1,15 @@
+package Week1;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import Week1.Week1ReviewedLab;
+import Week1.Performer;
 
 public class Week1ReviewedLabTest {
     @Test
     public void test(){
+        Performer test = new Performer();
+        Assert.assertNotNull(test);
     }
 }
