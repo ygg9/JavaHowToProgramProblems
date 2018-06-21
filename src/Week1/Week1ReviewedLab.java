3cce4b7a9ec7dc13b56885b71bebcd23bd8ef7f0
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-21 13:27:19 -0500

    refactored week1lab for readability and added junit test

--- src/Week1/Week1ReviewedLab.java
+++ src/Week1/Week1ReviewedLab.java
@@ -4,23 +4,42 @@ import java.util.ArrayList;
 
 public class Week1ReviewedLab {
     public static void main(String[] args){
-        for (int i = 0; i<4; i++){
+        int numPerformers = 4;
+        int numDancers = 2;
+        int numVocalist = 1;
+
+        printPerformers(numPerformers);
+        printDancers(numDancers);
+        printVocalists(numVocalist);
+
+    }
+
+    public static void printPerformers(int numberOfPerformers){
+        for (int i = 0; i<numberOfPerformers; i++){
             Performer jimmy = new Performer();
             System.out.println(jimmy.getId() + " - performer");
         }
-        for (int j = 0; j<2; j++){
+    }
+
+    public static void printDancers(int numberOfDancers){
+        for (int i = 0; i<numberOfDancers; i++){
             Dancer bob = new Dancer();
             System.out.println(bob.getStyle() + " - " + bob.getId() + " - dancer");
         }
-        int rand = (int)(Math.random() * 2 + 1);
-        if(rand == 1){
-            rand = (int)(Math.random() * 5 + 1);
-            Vocalist joe = new Vocalist(rand);
-            System.out.println("I sing in the key of - " + joe.getKey() + " - at the volume " + joe.getVolume() + " - "  + joe.getId());
-        }
-        else{
-            Vocalist joe = new Vocalist();
-            System.out.println("I sing in the key of - " + joe.getKey() + " - " + joe.getId());
+    }
+
+    public static void printVocalists(int numOfVocalists){
+        for (int i = 0; i<numOfVocalists; i++){
+            int rand = (int)(Math.random() * 2 + 1);
+            if(rand == 1){
+                rand = (int)(Math.random() * 5 + 1);
+                Vocalist joe = new Vocalist(rand);
+                System.out.println("I sing in the key of - " + joe.getKey() + " - at the volume " + joe.getVolume() + " - "  + joe.getId());
+            }
+            else{
+                Vocalist joe = new Vocalist();
+                System.out.println("I sing in the key of - " + joe.getKey() + " - " + joe.getId());
+            }
         }
     }
 }
