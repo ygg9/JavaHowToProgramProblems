b68767fa42bd43a5ac04d36e24c9377dde687a71
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-22 10:02:57 -0500

    refactored chapter8_4 into different classes

---
+++ src/Week2/Chapter8_10.java
@@ -0,0 +1,5 @@
+package Week2;
+
+public class Chapter8_10 {
+
+}
--- src/Week2/Chapter8_4.java
+++ src/Week2/Chapter8_4.java
@@ -15,7 +15,7 @@ public class Chapter8_4 {
         try{
             rect.setWidth(width);
         }
-        catch(outOfRange e){
+        catch(OutOfRange e){
 
         }
         System.out.println("Enter a height: ");
@@ -23,49 +23,8 @@ public class Chapter8_4 {
         try{
             rect.setHeight(height);
         }
-        catch(outOfRange e){
+        catch(OutOfRange e){
 
         }
-    }
-}
-
-class Rectangle{
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setWidth(double width) throws outOfRange {
-        if(width > 0.0 && width < 20.0){
-            this.width = width;
-        }
-        else{
-            throw new outOfRange("Width out of range. Must be between 0-20");
-        }
-    }
-
-    public void setHeight(double height) throws outOfRange {
-        if(height > 0.0 && height < 20.0){
-            this.height = height;
-        }
-        else{
-            throw new outOfRange("Height out of range. Must be between 0-20");
-        }
-    }
-
-    double width, height;
-    Rectangle(){
-        width = 1;
-        height = 1;
-    }
-
-}
-
-class outOfRange extends Exception{
-    outOfRange(String s){
-        System.out.println(s);
     }
 }
