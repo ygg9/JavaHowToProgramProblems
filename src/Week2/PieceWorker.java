d303b9c071af99b93bc19ff3069921523757456c
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-25 14:40:02 -0500

    Employee and pieceworker

---
+++ src/Week2/PieceWorker.java
@@ -0,0 +1,12 @@
+package Week2;
+
+public class PieceWorker extends Employee{
+    String firstName;
+    String lastName;
+    String SSN;
+
+    PieceWorker(String firstName, String lastName, String SSN){
+        super(firstName, lastName, SSN);
+
+    }
+}
