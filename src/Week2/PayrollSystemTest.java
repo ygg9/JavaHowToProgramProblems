4d9d4da2e58c10f32e340ad2ff4b0c63aa4fa8bf
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-26 10:33:34 -0500

    added 10.5-10.9

---
+++ src/Week2/PayrollSystemTest.java
@@ -0,0 +1,47 @@
+package Week2;
+
+public class PayrollSystemTest {
+    public static void main(String[] args){
+        SalariedEmployee salariedEmployee = new SalariedEmployee("John", "Smith", "111-11-1111", 800.00);
+        AbstractHourlyEmployee hourlyEmployee = new AbstractHourlyEmployee("Karen", "Price", "222-22-2222", 16.75, 40);
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Sue","Jones", "333-33-3333", 10000, .06);
+        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, .04, 300);
+
+        System.out.println("Employees processed individually:");
+
+        System.out.printf("%n%s%n%s: $%,.2f%n%n", salariedEmployee, "earned", salariedEmployee.earnings());
+
+        System.out.printf("%s%n%s: $%,.2f%n%n", hourlyEmployee, "earned", hourlyEmployee.earnings());
+
+        System.out.printf("%s%n%s: $%,.2f%n%n", commissionEmployee, "earned", commissionEmployee.earnings());
+
+        System.out.printf("%s%n%s: $%,.2f%n%n", basePlusCommissionEmployee, "earned", basePlusCommissionEmployee.earnings());
+
+        AbstractEmployee[] employees = new AbstractEmployee[4];
+
+        employees[0] = salariedEmployee;
+        employees[1] = salariedEmployee;
+        employees[2] = salariedEmployee;
+        employees[3] = salariedEmployee;
+
+        System.out.printf("Employees processed polymorphically:%n%n");
+
+        for(AbstractEmployee currentEmployee: employees){
+            System.out.println(currentEmployee);
+
+            if (currentEmployee instanceof BasePlusCommissionEmployee){
+                BasePlusCommissionEmployee employee = (BasePlusCommissionEmployee) currentEmployee;
+
+                employee.setBaseSalary(1.10 * employee.getBaseSalary());
+
+                System.out.printf("new base salary with 10%% increase is $%,.2f%n", employee.getBaseSalary());
+            }
+
+            System.out.printf("earned $%,.2f%n%n", currentEmployee.earnings());
+        }
+
+        for(int j= 0; j < employees.length; j++){
+            System.out.printf("Employee %d is a %s%n", j, employees[j].getClass().getName());
+        }
+    }
+}
