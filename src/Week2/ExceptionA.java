f7cdd045d30fcc1d27312c4b18f870cfe1621936
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-27 09:18:14 -0500

    added exception questions

---
+++ src/Week2/ExceptionA.java
@@ -0,0 +1,8 @@
+package Week2;
+
+public class ExceptionA extends Exception {
+    public ExceptionA(){
+        super("Exception A");
+    }
+
+}
---
+++ src/Week2/ExceptionB.java
@@ -0,0 +1,7 @@
+package Week2;
+
+public class ExceptionB extends Exception {
+    public ExceptionB(){
+        super("Exception B");
+    }
+}
