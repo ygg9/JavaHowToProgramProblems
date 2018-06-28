a960eba6ba814caaf8d720139193bc26cf4d086f
Anthony Lau <sf.hlp.plm.java.ygg9@gmail.com>
2018-06-28 13:36:55 -0500

    serialized

--- src/Week2/CreateMast.java
+++ src/Week2/CreateMast.java
@@ -1,23 +1,73 @@
 package Week2;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.lang.SecurityException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Formatter;
 import java.util.FormatterClosedException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class CreateMast {
+    private static ObjectOutputStream output;
     private static Formatter oldMaster;
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args){
-        openOldMastFile();
-        addMastRecords();
-        closeOldMastFile();
+        System.out.println("1 for serialized, 2 for text");
+
+        if(input.nextInt() == 1){
+            openOldMastFileSerialized();
+            addOldMastFileRecordsSerialized();
+            closeOldMastFileSerialized();
+        } else{
+            openOldMastFile();
+            addOldMastRecords();
+            closeOldMastFile();
+        }
     }
 
-    public static void addMastRecords(){
+    public static void closeOldMastFileSerialized(){
+        try{
+            if (output != null){
+                output.close();
+            }
+        } catch (IOException ioException){
+            System.err.println("Error closing file. Terminating");
+        }
+    }
+
+    public static void openOldMastFileSerialized(){
+        try {
+            output = new ObjectOutputStream(Files.newOutputStream(Paths.get("oldmast.ser")));
+        } catch (IOException ioException){
+            System.err.println("Error opening file. Terminating");
+            System.exit(1);
+        }
+    }
+
+    public static void addOldMastFileRecordsSerialized(){
+        System.out.printf("%s%n%s%n: ", "Enter account number, first name, last name, and balance.", "Enter end-of-file indicator to end input.");
+
+        while(input.hasNext()){
+            try{
+                Account account = new Account(input.nextInt(), input.next(), input.next(), input.nextDouble());
+                output.writeObject(account);
+            } catch (NoSuchElementException elementException){
+                System.err.println("Invalid input. Please try again.");
+                input.nextLine();
+            } catch (IOException ioException){
+                System.err.println("Error writing to file. Terminating");
+                break;
+            }
+            System.out.print(": ");
+        }
+    }
+
+    public static void addOldMastRecords(){
         System.out.printf("%s%n%s%n: ", "Enter account number, first name, last name, and balance.", "Enter end-of-file indicator to end input.");
 
         while(input.hasNext()){
